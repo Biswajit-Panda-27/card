850a5a4d46e30ed45009b7f3a754b399bd239010
Biswajit-Panda-27 <biswajitpanda94389@gmail.com>
2024-08-16 21:31:37 +0530

    submit

---
+++ script.js
@@ -0,0 +1,33 @@
+// <!--=========================== round shape card with hover effect ========================================-->
+
+const img = document.querySelector("img");
+const icons = document.querySelector(".icons-1");
+img.onclick = function () {
+    this.classList.toggle("active");
+    icons.classList.toggle("active");
+}
+
+
+// ======================================= Lab Alumni ==============================
+
+/*=============== SHOW SOCIAL NETWORKS ===============*/
+const showSocial = (toggleCard, socialCard) => {
+    const toggle = document.getElementById(toggleCard),
+        social = document.getElementById(socialCard)
+
+    toggle.addEventListener('click', () => {
+        // If the animation class exists, we add the down-animation class
+        if (social.classList.contains('animation')) {
+            social.classList.add('down-animation')
+
+            // We remove the down-animation class
+            setTimeout(() => {
+                social.classList.remove('down-animation')
+            }, 1000)
+        }
+
+        // We add the animation class to the div tag with the card__social class
+        social.classList.toggle('animation')
+    })
+}
+showSocial('card-toggle', 'card-social')
